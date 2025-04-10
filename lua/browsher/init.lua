1561ba1e55d412bcb3080afd359bf164652250e8
justiceenunciate <no@email.com>
2025-04-10 12:47:32 -0400

    fix open command by switching osx and windows

--- lua/browsher/init.lua
+++ lua/browsher/init.lua
@@ -18,10 +18,10 @@ local function get_open_command()
         end
     end
 
-    if vim.fn.has("unix") == 1 then
-        return { "xdg-open" }
-    elseif vim.fn.has("macunix") == 1 then
+    if vim.fn.has("macunix") == 1 then
         return { "open" }
+    elseif vim.fn.has("unix") == 1 then
+        return { "xdg-open" }
     elseif vim.fn.has("win32") == 1 then
         return { "explorer.exe" }
     else
